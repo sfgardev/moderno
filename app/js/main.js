a6a24b0d57940e9d66aeb4c49cd6095b85cb0c3b
sfgardev <sf.garol@gmail.com>
2019-12-04 22:04:24 +0500

    added product content

--- app/js/main.js
+++ app/js/main.js
@@ -25,14 +25,23 @@ $(function(){
 
     $('.icon-th-list').on('click', function(){
       $('.product__item').addClass('list');
-      $('.icon-th-list').addClass('active');
+      $(this).addClass('active');
       $('.icon-th-large').removeClass('active');
     });
     $('.icon-th-large').on('click', function(){
       $('.product__item').removeClass('list');
-      $('.icon-th-large').addClass('active');
+      $(this).addClass('active');
       $('.icon-th-list').removeClass('active');
     });
+
+    $('.product-one__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+      });
 
 
     $('.menu__btn').on('click', function(){
